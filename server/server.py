b6f6bc2dbf5c7f000d606e41aeee25041c8588ed
Alizaf <161627817+Alizalazar9@users.noreply.github.com>
2026-06-03 21:01:28 +0300

    port

--- server/server.py
+++ server/server.py
@@ -315,7 +315,7 @@ def internal_error(e):
 
 def open_browser():
     """Open default browser to the app (after short delay)."""
-    webbrowser.open("http://127.0.0.1:5000")
+    webbrowser.open("http://127.0.0.1:5001")
 
 
 if __name__ == "__main__":
